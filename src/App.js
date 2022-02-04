1742ed83050aa3e43f99af1357e032318eb2215d
Prakhar Dhyani <prakhardhyani1000@gmail.com>
2022-02-04 11:19:02 +0530

    starting with firebase

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import Cart from "./Components/Cart";
 import Navbar from "./Components/Navbar";
-
+import { collection, getDocs , getFirestore } from "firebase/firestore"; 
 
 
 
@@ -9,31 +9,10 @@ class App extends Component {
   constructor(){
     super()
     this.state = {
-       products:[
-           {
-             price: 99999,
-             title:"Mobile Phone" ,
-            quantity: 1,
-            img : "https://images.unsplash.com/photo-1591337676887-a217a6970a8a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=580&q=80",
-            id : 1
-            },
-           { 
-            price: 10000,
-            title:"Watch" ,
-            quantity: 5,
-            img : "https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/FWW12?wid=1144&hei=1144&fmt=jpeg&qlt=80&.v=1580325951651",
-            id : 2
-            },
-           {
-            price: 150000,
-            title:"laptop" ,
-            quantity: 5,
-            img : "https://images.unsplash.com/photo-1517336714731-489689fd1ca8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1026&q=80",
-            id : 3
-            }
-    ]
+       products:[]
     }
 }
+
 handleInc = (product)=>{
     const  products  = this.state.products;
     let index = products.indexOf(product);
--- src/index.js
+++ src/index.js
@@ -2,7 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { initializeApp } from "firebase/app";
 
+const firebaseConfig = {
+  apiKey: "AIzaSyAZzulDUjGS_eFDypaWJvkUXS2IZ7MEeOk",
+  authDomain: "cartapp-4f710.firebaseapp.com",
+  projectId: "cartapp-4f710",
+  storageBucket: "cartapp-4f710.appspot.com",
+  messagingSenderId: "88618188013",
+  appId: "1:88618188013:web:4e2092b7c73e86350e60e4"
+};
+
+
+
+const app = initializeApp(firebaseConfig);
 
 ReactDOM.render(
     <App />,
